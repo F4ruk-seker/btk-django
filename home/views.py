ce47fa9c869d7233d6e282674943e8886db2addf
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-02 13:59:57 +0300

    meta tag sync

--- home/views.py
+++ home/views.py
@@ -1,6 +1,16 @@
 from django.views.generic import TemplateView
+from home.models import Settings
 
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    @staticmethod
+    def get_page():
+        return Settings.objects.first()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page'] = self.get_page()
+        return context
 

a6e2a4056fda1ee657797bd921c3216dca792850
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-17 13:50:24 +0300

    Fix context

--- product/context_processors.py
+++ product/context_processors.py
@@ -2,6 +2,7 @@ from .models import Category
 
 
 def categories(request):
-    if categories := Category.objects.all():
-        return {'categories': categories}
+    if _ := Category.objects.all():
+        return {'categories': _}
     return {'categories': None}
+

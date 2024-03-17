55d377243265a696f355d7474e2152dfb3d6dc03
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-17 11:54:21 +0300

    cros url

--- order/urls.py
+++ order/urls.py
@@ -1,9 +1,4 @@
-from django.urls import path
-
-print('new')
-from order import views
-
-
+'''
 urlpatterns: list = [
     path('add-favorites', views.AddFavoriteView.as_view(), name='add_favorites'),
     path('favorites', views.MyFavoritesView.as_view(), name='favorites'),
@@ -12,3 +7,16 @@ urlpatterns: list = [
 urlpatterns.append(path("addtocart/<int:id>", views.addtocart, name="addtocart"))
 urlpatterns.append(path("shopcart", views.ShopCartDetailView.as_view(), name="shopocart"))
 urlpatterns.append(path("shopcart/<task>/<pk>", views.ShopCartDetailView.as_view(), name="shopocart-task"))
+
+'''
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('add-favorites', views.AddFavoriteView.as_view(), name='add_favorites'),
+    path('favorites', views.MyFavoritesView.as_view(), name='favorites'),
+    path("addtocart/<int:id>", views.addtocart, name="addtocart"),
+    path("shopcart", views.ShopCartDetailView.as_view(), name="shopocart"),
+    path("shopcart/<task>/<pk>", views.ShopCartDetailView.as_view(), name="shopocart-task"),
+]

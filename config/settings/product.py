f575c90ee30cda282bf95b2f08daf3f1bbc84e96
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-17 11:39:14 +0300

    add debugs

--- config/settings/product.py
+++ config/settings/product.py
@@ -1,8 +1,15 @@
 from .base import *
+import logging
 
+
+logger = logging.getLogger(__name__)
+
+logger.error('pars')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+logger.error(f'sD{DEBUG}')
+
 #
 # if DEBUG:
 #     CSRF_TRUSTED_ORIGINS: list = [f'http://{domain}' for domain in env.list('DJANGO_ALLOWED_HOSTS')]
@@ -12,6 +19,8 @@ CSRF_TRUSTED_ORIGINS: list = [f'https://{domain}' for domain in env.list('DJANGO
 # CSRF_TRUSTED_ORIGINS: list = env.list('DJANGO_ALLOWED_HOSTS')
 
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
+logger.error(f'sD{CSRF_TRUSTED_ORIGINS}')
+logger.error(f'sD{ALLOWED_HOSTS}')
 
 
 # Database

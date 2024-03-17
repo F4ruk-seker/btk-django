df89f9c5a475664063ff2f37b07395ccafdb0690
F4ruk-seker <ahmetfaruk731@gmail.com>
2024-03-17 13:53:03 +0300

    ADD whitenoise

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,8 @@ MIDDLEWARE = [
     # your middleware
 ]+[
     # third party middleware
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+
 ]
 
 
@@ -139,6 +141,7 @@ CKEDITOR_JQUERY_URL = 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
 # settings.py
 import sentry_sdk
 
